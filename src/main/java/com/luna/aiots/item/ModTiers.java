4d1dc20ee07d753dc4c2ca7a38c8cb0507849489
BloodMoonPhoenix <81944135+BloodMoonPhoenix@users.noreply.github.com>
2022-01-06 12:54:12 -0700

    Armor and Tools

---
+++ src/main/java/com/luna/aiots/item/ModTiers.java
@@ -0,0 +1,11 @@
+package com.luna.aiots.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.Tags;
+
+public class ModTiers {
+    public static final ForgeTier IRIDIUM = new ForgeTier(4, 3000, 12f, 4f,
+            15, Tags.Blocks.NEEDS_GOLD_TOOL,
+            () -> Ingredient.of(moditems.IRIDIUM_INGOT.get()));
+}
--- src/main/java/com/luna/aiots/item/moditems.java
+++ src/main/java/com/luna/aiots/item/moditems.java
@@ -1,8 +1,8 @@
 package com.luna.aiots.item;
 
 import com.luna.aiots.Aiots;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,6 +20,46 @@ public static final RegistryObject<Item> IRIDIUM_INGOT = ITEMS.register("iridium
 
     public static final RegistryObject<Item> RAW_IRIDIUM = ITEMS.register("raw_iridium",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_SWORD = ITEMS.register("iridium_sword",
+            () -> new SwordItem(ModTiers.IRIDIUM, 4, 4f,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_PICKAXE = ITEMS.register("iridium_pickaxe",
+            () -> new PickaxeItem(ModTiers.IRIDIUM, 1, 2f,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_SHOVEL = ITEMS.register("iridium_shovel",
+            () -> new ShovelItem(ModTiers.IRIDIUM, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_AXE = ITEMS.register("iridium_axe",
+            () -> new AxeItem(ModTiers.IRIDIUM, 6, 5,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_HOE = ITEMS.register("iridium_hoe",
+            () -> new HoeItem(ModTiers.IRIDIUM, 1, 2f,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_BOOTS = ITEMS.register("iridium_boots",
+            () -> new ArmorItem(ModArmorMaterial.IRIDIUM, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_LEGGINGS = ITEMS.register("iridium_leggings",
+            () -> new ArmorItem(ModArmorMaterial.IRIDIUM, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_CHESTPLATE = ITEMS.register("iridium_chestplate",
+            () -> new ArmorItem(ModArmorMaterial.IRIDIUM, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+    public static final RegistryObject<Item> IRIDIUM_HELMET = ITEMS.register("iridium_helmet",
+            () -> new ArmorItem(ModArmorMaterial.IRIDIUM, EquipmentSlot.HEAD,
+                    new Item.Properties().tab(ModCreativeModeTab.AIOTS_TAB)));
+
+
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
